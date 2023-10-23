bde6316ac6256bc9bb2be50306a2d3689d65c921
Enes <enesibisss0@gmail.com>
2023-10-23 23:55:34 +0300

    conditionals

---
+++ conditionals/conditionals/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int sayi = 10;
+        if (sayi<20){
+            System.out.println("Sayi 20 den küçüktür.");
+        }
+        else if (sayi<30){
+            System.out.println("Sayi 20 ile 30 arasındadır.");
+        }
+        else {
+            System.out.println("Sayi 30 dan büyüktür.");
+        }
+    }
+}
